dc69c98769aa619b2ea867692469d381d564bdc2
LoicRlm <ciwei9238@163.com>
2026-05-12 15:15:06 +0800

    Initial Commit

---
+++ Gemod for Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gemod for Mac
+//
+//  Created by Loic on 2026/5/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gemod for Mac/Gemod_for_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gemod_for_MacApp.swift
+//  Gemod for Mac
+//
+//  Created by Loic on 2026/5/12.
+//
+
+import SwiftUI
+
+@main
+struct Gemod_for_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
